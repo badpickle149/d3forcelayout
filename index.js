24db2cfb5bb942599890e320b4434cb8343f6d66
badpickle149 <kevinweng95@gmail.com>
2019-11-14 12:55:08 -0800

    inital files

---
+++ index.js
@@ -0,0 +1,180 @@
+
+const width = 1200, 
+        height = 800
+
+d3.csv('data.csv').then((data) => {
+    data = data.filter(d => d.source !== 'na')
+    console.log(data)
+    
+    // get unique names
+    const names = []
+
+    for (let i = 0; i < data.length; i++) {
+        const name1 = data[i].source
+        const name2 = data[i].target
+       
+        if (!names.includes(name1)) {
+            names.push(name1)
+        }
+
+        if (!names.includes(name2)) {
+            names.push(name2)
+        }
+    }
+
+    const colors = d3.scaleOrdinal()
+        .domain(names)
+        .range(d3.schemeSet2)
+
+    const nameToIndex = {}
+    names.forEach((d,i) => {
+        nameToIndex[d] = i
+    })
+
+    data = data.map((d) => {
+        return {
+            source: nameToIndex[d.source],
+            target: nameToIndex[d.target]
+        }
+    })
+    
+    const nodes = names.map((name, i) => { 
+        return {
+            "id": name,
+            index: i,
+            x: getRandomBetween(width/3, width*2/3),
+            y: getRandomBetween(height/3, height*2/3)
+        }
+    })
+
+    const svg = d3.select('body').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+
+    const nameNodes = svg.selectAll('circle')
+        .data(nodes)
+        .enter()
+        .append('text')
+            .attr('x', d => d.x)
+            .attr('y', d => d.y)
+            .text(d => d.id)
+            .style('font-size', '14pt')
+            .style('color', 'steelblue')
+
+        
+
+    
+
+    // let linkForce = d3.forceLink(data).distance(300).strength(1)
+
+    // let simulation = d3.forceSimulation(nodes).alphaDecay(0.01).force("linkForce",linkForce);
+    // simulation.on("tick", () => {
+    //     nameNodes.attr('x', d => d.x)
+    //         .attr('y', d => d.y)
+    // })
+
+    var simulation = d3.forceSimulation(nodes)
+  .force('charge', d3.forceManyBody().strength(-100))
+  .force('center', d3.forceCenter(width / 2, height / 2))
+  .force('link', d3.forceLink().distance(150).links(data))
+  .on('tick', ticked);
+
+  let iter = 0;
+
+function updateLinks() {
+    const plotNames = []
+
+  var u = svg
+    .selectAll('line')
+    .data(data)
+
+  u.enter()
+    .append('line')
+    .merge(u)
+    .attr('x1', function(d) {
+      return d.source.x
+    })
+    .attr('y1', function(d) {
+      return d.source.y
+    })
+    .attr('x2', function(d) {
+      return d.target.x
+    })
+    .attr('y2', function(d) {
+      return d.target.y
+    })
+    .style('stroke', d => {
+        if (iter > 0) {
+            if (!plotNames.includes(d.source.id)) {
+                plotNames.push(d.source.id)
+            }
+        }
+        iter++
+        return colors(d.source.id)
+    })
+    .style('opacity', 0.8)
+    
+    if (iter == data.length) {
+        svg.selectAll("mydots")
+        .data(plotNames)
+        .enter()
+        .append("circle")
+            .attr("cx", width - 100)
+            .attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+            .attr("r", 7)
+            .style("fill", function(d){ return colors(d)})
+
+        // Add one dot in the legend for each name.
+        svg.selectAll("mylabels")
+            .data(plotNames)
+            .enter()
+            .append("text")
+                .attr("x", width - 100)
+                .attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+                .style("fill", function(d){ return colors(d)})
+                .text(function(d){ return d})
+                .attr("text-anchor", "left")
+                .style("alignment-baseline", "middle")
+    }
+  
+    u.exit().remove()
+}
+
+function updateNodes() {
+  u = svg
+    .selectAll('text')
+    .data(nodes)
+
+  u.enter()
+    .append('text')
+    .text(function(d) {
+      return d.id
+    })
+    .merge(u)
+    .attr('x', function(d) {
+      return d.x
+    })
+    .attr('y', function(d) {
+      return d.y
+    })
+    .attr('dy', function(d) {
+      return 5
+    })
+    
+
+  u.exit().remove()
+}
+
+function ticked() {
+  updateLinks()
+  updateNodes()
+}
+
+
+
+
+})
+
+function getRandomBetween(x,y) {
+    return Math.floor(Math.random()*y) + x
+}
